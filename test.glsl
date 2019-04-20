fb7662423056af3f2a903fe81d1c504d32ad35e5
Rui Esteves <ruicarest@gmail.com>
2019-04-20 12:30:45 +0100

    Sine and Cosine + index

--- test.glsl
+++ test.glsl
@@ -2,18 +2,38 @@
 precision mediump float;
 #endif
 
+#define PI 3.14159265359
+
 uniform vec2 u_resolution;  // Canvas size (width,height)
 uniform vec2 u_mouse;       // mouse position in screen pixels
 uniform float u_time;       // Time in seconds since load
 
-void main() {
-
-	// //gl_FragColor = vec4(1.0,0.0,1.0,1.0);
-    // gl_FragColor = vec4(abs(sin(u_time))*0.5,abs(sin(u_time)),abs(sin(u_time)),1.0);
-	vec2 st = gl_FragCoord.xy/u_resolution;
-    float post = u_mouse.y / u_resolution.y;
-    float post2 = u_mouse.x / u_resolution.x;
-	//gl_FragColor = vec4(post,post2,0.0,1.0);
-
-    gl_FragColor = vec4(abs(sin(u_time*post)),abs(sin(u_time*post2)),0.0,1.0);
+float plot(vec2 st, float pct){
+  return  smoothstep( pct-0.02, pct, st.y) -
+          smoothstep( pct, pct+0.02, st.y);
 }
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+
+    //float y = pow(st.x,5.0);
+    //float y = step(0.5,st.x);
+    //float y = smoothstep(0.1,0.9,st.x);
+    float y = smoothstep(0.2,0.5,st.x) - smoothstep(0.5,0.8,st.x);
+    vec3 color = vec3(y);
+
+    float pct = plot(st,y);
+    color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+
+    gl_FragColor = vec4(color,1.0);
+}
+
+//y = mod(x,0.5); // return x modulo of 0.5
+//y = fract(x); // return only the fraction part of a number
+//y = ceil(x);  // nearest integer that is greater than or equal to x
+//y = floor(x); // nearest integer less than or equal to x
+//y = sign(x);  // extract the sign of x
+//y = abs(x);   // return the absolute value of x
+//y = clamp(x,0.0,1.0); // constrain x to lie between 0.0 and 1.0
+//y = min(0.0,x);   // return the lesser of x and 0.0
+//y = max(0.0,x);   // return the greater of x and 0.0 
