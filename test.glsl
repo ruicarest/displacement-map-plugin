19043bdaed2ad2686721596a061f8ac32f17d786
Rui Esteves <ruicarest@gmail.com>
2019-04-20 12:05:26 +0100

    Uniforms

--- test.glsl
+++ test.glsl
@@ -7,5 +7,13 @@ uniform vec2 u_mouse;       // mouse position in screen pixels
 uniform float u_time;       // Time in seconds since load
 
 void main() {
-	gl_FragColor = vec4(1.0,0.0,1.0,1.0);
+
+	// //gl_FragColor = vec4(1.0,0.0,1.0,1.0);
+    // gl_FragColor = vec4(abs(sin(u_time))*0.5,abs(sin(u_time)),abs(sin(u_time)),1.0);
+	vec2 st = gl_FragCoord.xy/u_resolution;
+    float post = u_mouse.y / u_resolution.y;
+    float post2 = u_mouse.x / u_resolution.x;
+	//gl_FragColor = vec4(post,post2,0.0,1.0);
+
+    gl_FragColor = vec4(abs(sin(u_time*post)),abs(sin(u_time*post2)),0.0,1.0);
 }
