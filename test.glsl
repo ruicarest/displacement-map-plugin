00ef3633bf9e3ae5d0f369f5b6691c11f88c3f75
Rui Esteves <ruicarest@gmail.com>
2019-04-21 22:22:11 +0100

    Spining

--- test.glsl
+++ test.glsl
@@ -20,17 +20,29 @@ vec3 hsb2rgb( in vec3 c ){
     return c.z * mix(vec3(1.0), rgb, c.y);
 }
 
+vec3 hsb2ryb( in vec3 c ){
+    // "Stretch" colors such that yellow hue is now opposite blue.
+    c.x = pow(c.x, 1.5);
+    // Rest of hsb to rgb implementation is the same.
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+                             6.0)-3.0)-1.0, 
+                     0.0, 
+                     1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix( vec3(1.0), rgb, c.y);
+}
+
 void main(){
     vec2 st = gl_FragCoord.xy/u_resolution;
     vec3 color = vec3(0.0);
 
     // Use polar coordinates instead of cartesian
     vec2 toCenter = vec2(0.5)-st;
-    float angle = sin(u_time*atan(toCenter.y,toCenter.x));
-    float radius = sin(length(toCenter)*2.0);
-
-    // We map x (0.0 - 1.0) to the hue (0.0 - 1.0)
-    // And the y (0.0 - 1.0) to the brightness
+    float angle = atan(toCenter.y,toCenter.x) + u_time * TWO_PI * 0.2 ;
+    float radius = length(toCenter)*2.0;
+  
+    // Map the angle (-PI to PI) to the Hue (from 0 to 1)
+    // and the Saturation to the radius
     color = hsb2rgb(vec3((angle/TWO_PI)+0.5,radius,1.0));
 
     gl_FragColor = vec4(color,1.0);
