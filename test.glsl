d742a31eaa838c7d58ea92a216c71b221f1d73ec
Rui Esteves <ruicarest@gmail.com>
2019-04-23 12:21:41 +0100

    Shapes - Exercises

--- test.glsl
+++ test.glsl
@@ -9,41 +9,25 @@ uniform vec2 u_resolution;  // Canvas size (width,height)
 uniform vec2 u_mouse;       // mouse position in screen pixels
 uniform float u_time;       // Time in seconds since load
 
-//  Function from Iñigo Quiles
-//  https://www.shadertoy.com/view/MsS3Wc
-vec3 hsb2rgb( in vec3 c ){
-    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
-                             6.0)-3.0)-1.0,
-                     0.0,
-                     1.0 );
-    rgb = rgb*rgb*(3.0-2.0*rgb);
-    return c.z * mix(vec3(1.0), rgb, c.y);
-}
 
-vec3 hsb2ryb( in vec3 c ){
-    // "Stretch" colors such that yellow hue is now opposite blue.
-    c.x = pow(c.x, 1.5);
-    // Rest of hsb to rgb implementation is the same.
-    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
-                             6.0)-3.0)-1.0, 
-                     0.0, 
-                     1.0 );
-    rgb = rgb*rgb*(3.0-2.0*rgb);
-    return c.z * mix( vec3(1.0), rgb, c.y);
-}
 
 void main(){
-    vec2 st = gl_FragCoord.xy/u_resolution;
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
     vec3 color = vec3(0.0);
 
-    // Use polar coordinates instead of cartesian
-    vec2 toCenter = vec2(0.5)-st;
-    float angle = atan(toCenter.y,toCenter.x) + u_time * TWO_PI * 0.2 ;
-    float radius = length(toCenter)*2.0;
-  
-    // Map the angle (-PI to PI) to the Hue (from 0 to 1)
-    // and the Saturation to the radius
-    color = hsb2rgb(vec3((angle/TWO_PI)+0.5,radius,1.0));
+    // bottom-left
+    //vec2 bl = step(vec2(0.05),st);
+    //vec2 bl = smoothstep(0.05, 0.1, st);
+    vec2 bl = vec2(floor(st-0.9));
+    float pct = bl.x * bl.y;
+
+    // top-right
+    //vec2 tr = step(vec2(0.05),1.0-st);
+    //vec2 tr = smoothstep(0.05, 0.1, 1.0-st);
+    vec2 tr = vec2(floor(0.1-st));
+    pct *= tr.x * tr.y;
+
+    color = vec3(pct);
 
     gl_FragColor = vec4(color,1.0);
 }
